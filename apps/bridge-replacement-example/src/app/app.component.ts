8b3480de03bbf28a8a7fd7de9475000be48e13ca
Samuel Barney <samjbarney@gmail.com>
2021-08-17 09:24:47 -0600

    initial commit

---
+++ apps/bridge-replacement-example/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bridge-replacement-example-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'bridge-replacement-example';
+}
